e6405671bd48e95bb3dcd7ef60c0473e10cbfcbc
Kristof Van Laerhoven <kryztov@gmail.com>
2024-04-12 07:27:26 +0200

    Create example05.cpp

---
+++ basic_statements/example05.cpp
@@ -0,0 +1,13 @@
+/**
+ Write a program that prints in the terminal all prime numbers from 3 till 99.   
+ Remember: A number is a prime when any division by a smaller number results in
+ a remainder that is never zero.
+*/
+#include <iostream>  // to allow use of std::cout and std::endl
+int main( ) {
+
+
+
+
+  return 0;
+}
