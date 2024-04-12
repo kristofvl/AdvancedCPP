2d75cb383bbbcfa21ee844ac7c1b1bfa176bbbe9
Kristof Van Laerhoven <kryztov@gmail.com>
2024-04-12 07:26:54 +0200

    Create example04.cpp

---
+++ basic_statements/example04.cpp
@@ -0,0 +1,13 @@
+/**
+  Write a program that counts from 131 down till 23, one number per line in the  
+  the terminal, and prints out "hop", if the number is a multiple of 7. 
+  */
+#include <iostream>  // to allow use of std::cout and std::endl
+int main( ) {
+    
+  
+
+
+
+  return 0;
+}
