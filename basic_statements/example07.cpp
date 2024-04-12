2c8042c4d22e9ac0bf38e20c4038c1afedc310ac
Kristof Van Laerhoven <kryztov@gmail.com>
2024-04-12 07:28:11 +0200

    Create example07.cpp

---
+++ basic_statements/example07.cpp
@@ -0,0 +1,16 @@
+/**
+ Write a program that draws in the terminal a bigger X out of the character 'X',  
+ depending on the variable int size (with size = 3, 4, ..., 20):
+ size = 3:     size = 4:      size = 5:      etc.
+   XX X         XX  X          XX   X
+    XX           XXX            XXXX
+   X XX          XXX             XX
+                X  XX           X XX
+                               X   XX      */
+#include <iostream>  // to allow use of std::cout and std::endl
+int main( ) {
+
+
+
+  return 0;
+}
