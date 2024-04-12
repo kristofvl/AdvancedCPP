745a206e6ccdb84617a4f5b4b66beab921731ca3
Kristof Van Laerhoven <kryztov@gmail.com>
2024-04-12 07:24:55 +0200

    Create example01.cpp

---
+++ basic_statements/example01.cpp
@@ -0,0 +1,13 @@
+/**
+  Write a program that initializes two integers 'number1' and 'number2', 
+  and tests whether number1 is a multiple of number2. Put this result in the 
+  boolean variable 'result' below.
+  */
+
+int main() {
+  bool result = false; 
+  
+
+
+  return (int) result;
+}
