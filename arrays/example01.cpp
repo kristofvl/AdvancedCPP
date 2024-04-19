c7e369603c140d8b1ae3847d4cc79f74f7f92c0c
Kristof Van Laerhoven <kryztov@gmail.com>
2024-04-19 08:17:26 +0200

    Create example01.cpp

---
+++ arrays/example01.cpp
@@ -0,0 +1,14 @@
+/**
+  Write a program that initializes an array of 50 booleans, repeatedly having two
+  elements with a true value, followed by one element with false.
+  So the array starts with: true, true, false, true, true, false, true, true, … 
+  Do not use any variables other than myArray. 
+*/
+
+int main() {
+  bool myArray[50]; 
+  
+
+
+  return 0;
+}
