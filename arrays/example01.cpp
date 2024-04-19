9c87a730e4c618498693d5ee5e689c45a5717545
Kristof Van Laerhoven <kryztov@gmail.com>
2024-04-19 17:50:42 +0200

    Update example01.cpp

--- arrays/example01.cpp
+++ arrays/example01.cpp
@@ -2,7 +2,7 @@
   Write a program that initializes an array of 50 booleans, repeatedly having two
   elements with a true value, followed by one element with false.
   So the array starts with: true, true, false, true, true, false, true, true, … 
-  Do not use any variables other than myArray. 
+  Do not use any variables other than myArray and a loop iteration variable. 
 */
 
 int main() {
