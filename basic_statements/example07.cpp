b47f0912aba77e60df08f0eb1c658b8b760cc16d
Kristof Van Laerhoven <kryztov@gmail.com>
2024-04-19 14:35:22 +0200

    Update example07.cpp

--- basic_statements/example07.cpp
+++ basic_statements/example07.cpp
@@ -3,7 +3,7 @@
  depending on the variable int size (with size = 3, 4, ..., 20):
  size = 3:     size = 4:      size = 5:      etc.
    XX X         XX  X          XX   X
-    XX           XXX            XXXX
+    XX           XXX            XX X
    X XX          XXX             XX
                 X  XX           X XX
                                X   XX      */
