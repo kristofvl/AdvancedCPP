996e76cb73cc021ea3b2ce591918004b44ad53ff
Kristof Van Laerhoven <kryztov@gmail.com>
2024-04-22 08:27:33 +0200

    Create example00.cpp

---
+++ classes/example00.cpp
@@ -0,0 +1,14 @@
+/**
+  Write a program that declares, implements, and uses a class with no attributes.
+  The class should print 'hello' to the terminal when its object is created and
+  'bye' when its object is removed from memory.
+*/
+#include <iostream>     // terminal input and output classes and objects
+
+// write the class here
+
+int main() {
+  // create a class object here
+
+  return 0;
+}
