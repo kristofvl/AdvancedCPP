7fb8065f06e7430782813d919be1f64cec14c5a5
Kristof Van Laerhoven <kryztov@gmail.com>
2024-04-25 16:50:17 +0200

    Create example02.cpp

---
+++ classes/example02.cpp
@@ -0,0 +1,17 @@
+/**
+  Create a class with just a static member, and illustrate that it exists, even 
+  without any objects.
+*/
+
+#include <iostream>
+
+// write the class here
+// class …
+
+int main() {
+  // do not create an object here, but assign a value to static member here 
+  std::cout << " The value of the static member is "; 
+  // print its value here
+  std::cout << std::endl;
+  return 0;
+}
