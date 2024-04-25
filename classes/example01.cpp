ea23094289301e617a1558e8dc65fe0536b4f793
Kristof Van Laerhoven <kryztov@gmail.com>
2024-04-25 16:22:11 +0200

    Create example01.cpp

---
+++ classes/example01.cpp
@@ -0,0 +1,14 @@
+/**
+  Write a program that declares, implements, and uses a class with two attributes,
+  a boolean called 'flag' and an integer called 'number', which can only be changed 
+  or read through a constructor. The class should also have a method 'get' with no 
+  parameters, which returns the integer 'number' only if 'flag' is true, and   
+  otherwise 0.
+*/
+// write the class here
+int main() {
+  int returnValue; 
+  // create a class object here 
+  // and use its get method
+  return returnValue;
+}
