2d54d14a4b7838c111ff3b92060f39f3714c4814
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-01 21:46:30 +0200

    Create example01.cpp

---
+++ pointers/example01.cpp
@@ -0,0 +1,15 @@
+/* Write a class, called Check, below to illustrate in the main function that
+   the pointer to an object of class Check b is indeed pointing to the object of
+   class Check a. */
+#include <iostream>
+
+// write the class Check here
+
+int main(int argc, char *argv[]) {
+  Check a;  // a is an object of class Check
+  Check *b = &a;  // assign address of a to pointer b to object of class Check
+  if ( b->isThisMe( &a ) ) {
+    std::cout << "&a is b \n";
+  }
+  return 0;
+}
