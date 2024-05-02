c3a5b07de26dca73c267e645510bc60a8ecfc676
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-02 16:39:14 +0200

    Update example03.cpp

--- pointers/example03.cpp
+++ pointers/example03.cpp
@@ -1,6 +1,6 @@
 /** Print a mouse in the console, using a const pointer to avoid changes */
 #include <iostream>     // terminal output
-auto *getBitmapAddress() {
+[[nodiscard]] auto *getBitmapAddress() {
   static char bitmap[] = "(^._.^)~";  // "bitmap" created in static memory
   return bitmap;  // return pointer to first element
 }
