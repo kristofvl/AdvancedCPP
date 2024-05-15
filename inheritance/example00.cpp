81e276d128b46ed891adc1d11429d484c5d15a7d
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-15 21:08:29 +0200

    Update example00.cpp

--- inheritance/example00.cpp
+++ inheritance/example00.cpp
@@ -1,21 +1,22 @@
-#include <iostream>
+#include <iostream> 
 
-class Rectangle {  // class Rectangle has width and height as attributes and area() as a method
+class Rectangle {  // class Rectangle has width and height as attributes and area() as a method  
  private:
-  int width, height;  // width and height are private, so not accessible from outside the class
+  int width, height;  // width and height are private, so not accessible from outside the class 
  public:
   Rectangle() {}  // default constructor, allows to define width and height later
-  Rectangle(int x, int y) width(x), height(y) {}  // constructor that sets attributes
+  Rectangle(int x, int y): width(x), height(y) {}  // constructor that sets attributes
   int area() { return width*height; };
-  // declare the friend method "enlarge()" here, with as parameter a rectangle
+  // declare the friend method "enlarge()" here, with as parameter a rectangle  
 };
 
-/* define the friend method here, so that it creates and returns a copy of the rectangle that
+/* define the friend method here, so that it creates and returns a copy of the rectangle that 
    has twice the width and height. The friend method has access to the private attributes. */
 
 int main() {
   Rectangle rectangle1, rectangle2(3,4);  // rectangle1 will obtain twice the width and height
-  rectangle1 = enlarge(rectangle2);        // of rectangle2 through the enlarge method
-  std::cout << rectangle1.area() << "\n";
-  return 0;
+  rectangle1 = enlarge(rectange2);        // of rectangle2 through the enlarge method
+  std::cout << rectangle1.area() << "\n";  // this should return "48" (6 times 8)
+  return 0;  
 }
+
