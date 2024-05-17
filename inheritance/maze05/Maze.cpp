460ea7d66fc7994d66b30eea9a69874358e81cfa
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-17 11:23:34 +0200

    Update Maze.cpp

--- inheritance/maze05/Maze.cpp
+++ inheritance/maze05/Maze.cpp
@@ -11,15 +11,15 @@ Maze::Maze(std::string mazeFile) {
     while (std::getline(fileData, str)) {
       if (str.length() > 1 && cols == 0)
         cols = std::count_if(str.begin(), str.end(),
-                             [](char c){return c==',';});
+                             [](char c){return c==',';}) + 1;
       lines++;
-    }
-    this->mazeXlen = cols+1; this->mazeYlen = lines;
+    }  // cols: count of first line's commas + 1, lines: count of lines
+    this->mazeXlen = cols; this->mazeYlen = lines;
 		// then fill the maze array:
     fileData.close();
     std::ifstream fileData(mazeFile);
     auto iter = 0;
-    maze = new uint8_t[mazeYlen*mazeXlen];
+    maze = new uint8_t[mazeYlen*mazeXlen];  // create and fill maze
     while (fileData >> symbol) {
       if (symbol=='1' || symbol=='0') {
         maze[iter] = (symbol-'0');
@@ -33,7 +33,6 @@ Maze::Maze(std::string mazeFile) {
   start_color();  // use color, with these color pairs:
   init_pair(1, COLOR_BLACK, COLOR_BLACK);   // coridors
   init_pair(2, COLOR_YELLOW, COLOR_GREEN);  // walls
-  init_pair(3, COLOR_WHITE, COLOR_BLACK);  // player
 }
 
 Maze::~Maze() {
@@ -42,7 +41,7 @@ Maze::~Maze() {
 }
 
 // (re)draw the maze with player as symbol, in colorpair:
-void Maze::draw(char symbol, int colorpair) {
+void Maze::draw(char symbol, int colorpair) const {
 	for ( auto line = 0; line < LINES; line++) {
     for ( auto col = 0; col < COLS; col++) {
       attron(COLOR_PAIR( get(col, line)? colorpair: 1));
