47622c94122248edd89597f257958cd88742cc8e
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-23 17:05:03 +0200

    Create example00.cpp

---
+++ streams/example00.cpp
@@ -0,0 +1,17 @@
+// change the following code to output the server reply straight to a local html file:
+#include <fstream>
+#include <boost/asio.hpp>
+int main() {
+  const int bufferSize = 4096;
+  boost::asio::ip::tcp::iostream socket("www.example.com", "http");  // socket stream
+  std::ofstream outputFile("myTest.txt");  // stream to output file
+  outputFile << "Reply of server:\n";
+  char reply[bufferSize];
+
+  socket << "GET / HTTP/1.1\r\nHost: www.example.com\r\nConnection: Close\r\n\r\n" ;
+  socket << std::flush;
+  socket.read(reply, bufferSize);
+
+  outputFile << reply;  // output the reply of server to our text file
+  return 0;
+}
