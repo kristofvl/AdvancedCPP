c40c66a24cece49bb7eda65487ce26939ca3915d
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-23 20:52:55 +0200

    Create example01.cpp

---
+++ containers/example01.cpp
@@ -0,0 +1,70 @@
+#include <iostream>  // std::cout, std::runtime_error, std::exception, std::cerr
+
+class QueueElement;  // declaration of the element class
+
+class UQueue {  // Class for an unlimited queue of QueueElements
+ public:
+  UQueue() { head = tail = nullptr; }
+  ~UQueue() { clear(); };
+  void put(int data);
+  int get();
+  bool isEmpty() const;
+  void clear();
+ private:
+  QueueElement * head;  // pointer to element to put
+  QueueElement * tail;  // pointer to element to get from
+};
+
+class QueueElement {  // element class, hidden from users
+ public:
+  QueueElement(int data) : data(data) , next(nullptr) {}
+  int data;
+  QueueElement * next;
+};
+
+void UQueue::clear() {  // iteratively clear the queue of all elements
+  QueueElement * elem, * elem_next;
+  for (elem = head; elem != nullptr; elem = elem_next) {
+    elem_next = elem->next;
+    delete elem;
+  }
+  head = tail = nullptr;
+}
+
+bool UQueue::isEmpty() const {  // check whether the queue is empty
+  return head == nullptr;
+}
+
+void UQueue::put(int data) {  // put in new data element
+  QueueElement *node = new QueueElement(data);
+  if (isEmpty()) {
+    head = tail = node;
+  } else {
+    tail = tail->next = node;  // tail is guaranteed to be valid
+  }
+}
+
+int UQueue::get() {  // get in new data element
+  int retVal;
+  if (!isEmpty()) {
+    retVal = head->data;
+    QueueElement *second = head->next;
+    delete head;
+    head = second;
+  } else {
+    throw std::runtime_error("uqueue: empty on get");
+  }
+  return retVal;
+}
+
+int main() {
+	UQueue q;
+	for (auto i = 0; i < 128; i++) {
+    q.put(i);
+  }
+  for (auto i = 0; i < 129; i++) {
+    std::cout << q.get() << " ";
+  }
+  std::cout << "\n";
+	return 0;
+}
