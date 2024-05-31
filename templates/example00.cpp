6176c38c1cd7ab1432dfd8ae48493def4071690d
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-31 10:13:05 +0200

    Update example00.cpp

--- templates/example00.cpp
+++ templates/example00.cpp
@@ -1,1 +1,61 @@
-// Vector Example
+// Implementation of the Queue from chapter 10 with template
+
+#include <iostream>  // use of std::cout, std::cin
+
+template <class Data> class Queue {  // Class for a queue of integers
+ public:
+  Queue(int size = 100) : maxSize(size), tail(0), head(0), filled(0) { items = new int[size]; }
+  ~Queue() { delete[] items; items = nullptr; };
+  void put(Data data);
+  Data get();
+  bool isFull() const { return filled == maxSize; }
+  bool isEmpty() const { return filled == 0; }
+  void clear() { filled = 0; head = 0; tail = 0; }  // clear whole queue
+ private:
+  Data *items;    // array of integers
+  int maxSize;   // size of items
+  int tail;      // position in array to put
+  int head;      // position in array to get from
+  int filled;    // number of elements in queue
+};
+
+// put element at the tail of the queue, for example put(17) updates:
+// items:  [  ][  ][  ][  ][  ][17][  ] … [  ][  ]
+//             head            tail→
+template <class Data>
+void Queue<Data>::put(Data data) {   // put element at tail
+  if (!isFull()) {
+    items[tail] = data;
+    tail = (tail+1) % maxSize;
+    filled++;
+  } else {
+    throw std::runtime_error("queue: full on put");
+  }
+}
+
+// gets element at the head of the queue, for example get() updates:
+// items:  [  ][  ][  ][  ][  ][  ][  ] … [  ][  ]
+//             head→           tail
+template <class Data>
+Data Queue<Data>::get() {  // get and remove element from head
+  Data retval;
+  if (!isEmpty()) {
+    retval = items[head];
+    head = (head+1) % maxSize;
+    filled--;
+  } else {
+    throw std::runtime_error("queue: empty on get");
+  }
+  return retval;
+}
+
+int main() {
+  Queue<int> q(127);
+  for (auto i = 0; i < 127; i++) {
+    q.put(i);
+  }
+  for (auto i = 0; i < 127; i++) {
+    std::cout << q.get() << " ";
+  }
+  return 0;
+}
