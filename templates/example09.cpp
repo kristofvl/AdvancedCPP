2464f96fa4613fcf710eb30f13e63ba4abd7795c
Kristof Van Laerhoven <kryztov@gmail.com>
2024-06-07 15:14:56 +0200

    Update example09.cpp

--- templates/example09.cpp
+++ templates/example09.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 
 int main() {
-  std::vector a{ 16.0, 17.0, 18.0 };  // since C++17
-  std::vector b{ 10.0, 30.0, 60.0 };
+  std::vector a( { 16.0, 17.0, 18.0 } );  // since C++17
+  std::vector b( { 10.0, 30.0, 60.0 } );
   std::vector<double> c;
   double n = 2.7;
   std::transform(a.begin(), a.end(),               // iterators for all
