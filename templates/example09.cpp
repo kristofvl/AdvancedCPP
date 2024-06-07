7f8cbc521e1daa68280bb9d6a625821f53a12efb
Kristof Van Laerhoven <kryztov@gmail.com>
2024-06-07 14:22:01 +0200

    Update example09.cpp

--- templates/example09.cpp
+++ templates/example09.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include <map>
+#include <vector>
 
 int main() {
-  std::vector a{ 16.0, 17.0, 18.0 };
+  std::vector a{ 16.0, 17.0, 18.0 };  // since C++17
   std::vector b{ 10.0, 30.0, 60.0 };
   std::vector<double> c;
   double n = 2.7;
-  transform(a.begin(), a.end(), b.begin(), std::back_inserter(c),  // iterators
+  std::transform(a.begin(), a.end(),               // iterators for all
+                 b.begin(), std::back_inserter(c), // input and output vectors
           [=](double x, double y) {return(x - y)/n; });
          // [=] : capture all external variables (n) in lambda by value
          // [&] : capture all external variables (n) in lambda by reference
-
   for (const double & i : c) {
     std::cout << i << "\n";
   }
   return 0;
 }
+
