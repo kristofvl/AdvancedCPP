672137bce1dcbfdbde92bab19d08e96fa990b40c
Kristof Van Laerhoven <kryztov@gmail.com>
2024-06-14 07:16:20 +0200

    Update Abstract.cpp

--- abstract/Abstract.cpp
+++ abstract/Abstract.cpp
@@ -2,14 +2,14 @@
 
 class AbstractClass {  // Class has pure virtual method:
  public:
-  virtual void printName() = 0;
+  virtual void printName() const = 0;
  protected:
   std::string name = "myName";
 };
 
 class DerivedClass : public AbstractClass {
- public:
-  virtual void printName() { std::cout << name << "\n"; }
+ public:  // printName is overridden and implemented here:
+  virtual void printName() const override  { std::cout << name << "\n"; }
 };
 
 int main() {
