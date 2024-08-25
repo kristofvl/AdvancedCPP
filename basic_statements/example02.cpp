9bd5edd2464e18f89fa0407db2e9654418b7477d
Aroosa Batool <Aroocabatool@gmail.com>
2024-08-25 23:53:21 +0200

    Update example02.cpp

--- basic_statements/example02.cpp
+++ basic_statements/example02.cpp
@@ -5,9 +5,8 @@
   */
 #include <iostream>  // to allow use of std::cout and std::endl
 int main( ) {
-      
-
-
-
+       for(double i =120.0; i>43.7; i-=7){
+       std:: cout <<i<< std:: endl;
+      }
   return 0;
 }
