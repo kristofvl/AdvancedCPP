21bc2d2cc0e6924391df1535bf3fbb24c4ab6568
Kristof Van Laerhoven <kvl@eti.uni-siegen.de>
2026-05-14 17:35:22 +0200

    added example06

--- 05_arrays/example06.cpp
+++ 05_arrays/example06.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
-//#include <algorithm>
 
 int main() {
     int myArray[] = { 54, 21, 8, 27, 10, 5, 30, 12 };
 
     // Use std::for_each to print all elements smaller
     // than 35 and sum these:
-    int sum = 0;
-    std::for_each(std::begin(myArray), std::end(myArray),
-        [&sum](int n) {
-            if (n < 35) {
-                sum += n;
-                std::cout << n << " ";
-            }
-        });
-    std::cout << "\n sum = " << sum << '\n';
+
+
 }
